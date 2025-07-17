87e1d0ce4fb53426b1adc612454973e5bf2c6bef
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-07-17 07:55:38 -0500

    Exercise 11

--- exercise11_primality_functions.py
+++ exercise11_primality_functions.py
@@ -1,4 +1,14 @@
-x = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-r = range(2, x)
+a = [x for x in range(2, num) if num % x == 0]
 
+def is_prime(n):
+    if num > 1:
+        if len(a) == 0:
+            print("Prime")
+        else:
+            print("Composite")
+    else:
+        print("Composite")
+
+is_prime(num)
