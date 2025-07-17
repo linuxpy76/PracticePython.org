49e5f8a959f5eeb4add908ee82d62625a6d56e66
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-07-17 13:23:22 -0500

    Exercise 13

--- exercise13_fibonacci.py
+++ exercise13_fibonacci.py
@@ -1,1 +1,18 @@
-num = int(input("How many numbers should the fibonacci sequence have?: "))
+def fibonacci():
+    num = int(input("How many do you want to generate for the fibonacci sequence?: "))
+    i = 1
+    if num == 0:
+        fib = []
+    elif num == 1:
+        fib = [1]
+    elif num == 2:
+        fib = [1,1]
+    elif num > 2:
+        fib = [1,1]
+        while i < (num - 1):
+            # print(fib)
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+    return fib
+
+print(fibonacci())
