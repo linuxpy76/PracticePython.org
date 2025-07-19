4f0bab701f216d6f489e15633f91df87086a3ac0
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-07-19 10:05:00 -0500

    Updating exercise 14

--- exercise14_remove_duplicates.py
+++ exercise14_remove_duplicates.py
@@ -1,20 +1,27 @@
 import random
 
-# Randomly generate lists
-
 list_length = 30
 min_value = 1
 max_value = 100
 
+# Randomly generate lists
 random1 = [random.randint(min_value, max_value) for _ in range(list_length)]
 print(sorted(random1))
-# random2 = [random.randint(min_value, max_value) for _ in range(list_length)]
+random2 = [random.randint(min_value, max_value) for _ in range(list_length)]
+print(sorted(random2))
 
-def remove_duplicates(v):
+def remove_duplicates_set(values):
+    return list(set(values))
 
-    set1 = set(v)
-    v = list(set1)
-    v.sort()
-    return(v)
+def remove_duplicates_loop(values):
+    list1 = []
+    
+    for i in values:
+        if i not in list1:
+            list1.append(i)
+    
+    list1.sort()
+    return list1
 
-print(remove_duplicates(random1))
+print(remove_duplicates_set(random1))
+print(remove_duplicates_loop(random2))
